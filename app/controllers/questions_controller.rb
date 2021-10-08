9d20730ed3a67090c7c0f712d6359740dd51c1d6
Alexandr <sashkam.98@mail.ru>
2021-10-08 22:36:00 +0300

    3 lesson 9:27

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,15 @@
 class QuestionsController < ApplicationController
+
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    render plain: params
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/questions', to: 'questions#create'
 get  '/questions', to: 'questions#index'
+get  '/questions/new', to: 'questions#new'
 root 'pages#index'
 
 end
