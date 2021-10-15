14c79984e7bfc0d801c776889c6d824838af0988
Alexan8r <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>
2021-10-15 13:26:02 +0300

    9 lesson 13:12 Исправить нумерацию страниц

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :users, only: %i[new create]
-
+  resources :users, only: %i[new create destroy]
+  resources :sessions, only: %i[new create]
   resources :questions do
     resources :answers, except: %i[new show]
   end
